ae11dac25d3f249ebbb6e0037bb3121fa7fdc94c
ghvasconcelos <ghvasconcelos@hotmail.com>
2023-05-04 14:29:43 -0300

    04/05

---
+++ Aula15/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.45;
+//let num2 = Math.floor(num1); ---> Arredonda pra baixo
+//let num2 = Math.ceil(num1); ---> Arredonda pra cima
+//let num2 = Math.round(num1); ---> Usa regra normal de arredondamento 0.5+ pra cima 0.5- pra baixo
+// console.log(Math.max(1,2,3,4,5,6,7,8,9)); ---> Pega o maior valor apresentado
+// console.log(Math.min(1,2,3,4,5,6,7,8,9)); ---> Pega o menor valor apresentado
+// console.log(Math.random()); ---> Numero aletorio entre 0-1 (1 não incluido)
+// console.log(Math.random() * (10 - 5) + 5 ); --->Numero aleatorio entre 10 e 5
+//Pode utilizar Math.round para arredondar por exemlo Math.round(Math.random());
+//Math.pow(2, 10);(2 Elevado a 10) ---> Potenciação ou pode se usar ** /// 2 ** 10
+//Para raiz quadrada pode se utilizar X ** (1/2)(ou 0.5);
