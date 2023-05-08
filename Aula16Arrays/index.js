68afabbcb8bc02b9afe7f93df001e798aa69cdf3
ghvasconcelos <ghvasconcelos@hotmail.com>
2023-05-08 14:14:01 -0300

    08 05

---
+++ Aula16Arrays/index.js
@@ -0,0 +1,18 @@
+
+//               0       1         2
+const alunos = ['Luiz', 'Maria', 'João']; //Arrays , é formado lista e fica de uma forma mais facil de dividir e alterar valores dentro dos arrays
+console.log(alunos);
+
+
+//alunos[3] = 'Luiza' -- Dessa forma eu adiciono um novo nome a lista. -- para isso é necessario saber o tamanho do array
+//pode se utilizar console.log(alunos.lenght) para saber o tamanho do array
+
+//alunos.push('Gustavo'); Dessa forma o array adiciona ao final da lista um novo nome
+//Sem a necessidade de saber quantos tem
+//alunos.unshift('Luiza'); //Dessa forma eu adiciono no inicio do array
+//alunos.pop(); -- Dessa forma remove o indice final do array 
+//alunos.shift(); -- Dessa forma remove-se do inicio
+//const removido = alunos.pop()//alunos.shift(); Dessa forma vc consegue salvar o indice removido
+
+//alunos[0] = 'Eduardo';  Dessa forma é possivel alterar o indice 0 (João) do array para Eduardo
+
