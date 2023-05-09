11d8cfcc7339a51212cab353f9c49c82f38c68d3
ghvasconcelos <ghvasconcelos@hotmail.com>
2023-05-09 11:49:54 -0300

    09 05

--- Aula16Arrays/index.js
+++ Aula16Arrays/index.js
@@ -3,16 +3,24 @@
 const alunos = ['Luiz', 'Maria', 'João']; //Arrays , é formado lista e fica de uma forma mais facil de dividir e alterar valores dentro dos arrays
 console.log(alunos);
 
+//Para verificar se é um array basta colocar console.log(alunos instanceof Array);
+
+//delete alunos[1]; -- Dessa forma eu apago o indice 1 ( Maria ) e la fica como <empty item>
 
 //alunos[3] = 'Luiza' -- Dessa forma eu adiciono um novo nome a lista. -- para isso é necessario saber o tamanho do array
 //pode se utilizar console.log(alunos.lenght) para saber o tamanho do array
 
 //alunos.push('Gustavo'); Dessa forma o array adiciona ao final da lista um novo nome
 //Sem a necessidade de saber quantos tem
+
 //alunos.unshift('Luiza'); //Dessa forma eu adiciono no inicio do array
+
 //alunos.pop(); -- Dessa forma remove o indice final do array 
+
 //alunos.shift(); -- Dessa forma remove-se do inicio
+
 //const removido = alunos.pop()//alunos.shift(); Dessa forma vc consegue salvar o indice removido
+
 
 //alunos[0] = 'Eduardo';  Dessa forma é possivel alterar o indice 0 (João) do array para Eduardo
 
---
+++ Aula17Funçoes/index.js
@@ -0,0 +1,30 @@
+//FUNÇÕES --- TUDO A BAIXO DE RETURN É IGNORADO A NÃO SER QUE TENHA UMA CONDICIONAL.
+//Não é necessario ; no final da declaração da funções a não ser em casos especificos como const !!! = function() {};
+
+// NOME --------------------------------------------
+/* function saudacao(nome){
+ console.log(`Bom dia ${nome}`);
+ return `Bom dia ${nome}`;
+} 
+
+const variavel = saudacao('Gustavo');
+console.log(variavel) */
+
+// SOMA --------------------------------------------
+/* function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(2, 2)); */
+
+/* const raiz = function(n){
+return n ** 0.5;
+};
+
+console.log(raiz(9)) */
+ //arrow function devido a seta
+/* const raiz = n => n ** 0.5;
+    
+console.log(raiz(9))  */
+
