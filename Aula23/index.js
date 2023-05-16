40a96b3c1d915d625940f091af101bdab3be30a5
ghvasconcelos <ghvasconcelos@hotmail.com>
2023-05-16 13:40:03 -0300

    16 05

---
+++ Aula23/index.js
@@ -0,0 +1,12 @@
+/*
+&& -> false && true -> false "o valor mesmo" caso true retorna o ultimo valor
+|| -> true || false -> vai retornar true ( o valor verdadeiro )
+
+FALSY - Valores falsos
+*false ->literal false
+0
+'' " " `` ->Expressões vazias
+null / undefined
+NaN
+Qualquer coisa fora disso é avaliado em True
+*/
--- aula21LOGICA/index.js
+++ aula21LOGICA/index.js
@@ -9,3 +9,10 @@
 !== Diferente Estrito (valor e tipo)
  */
 
+//> >= < <= retornam valores boolean (true ou false)
+
+// == e != não é comumente usado
+
+// === testa tanto o valor (escrito ou numero) e o tipo (string number etc) para ver se é igual e retorna um valor boolean
+
+// !== da mesma forma do === porem checa se são diferentes ou não
